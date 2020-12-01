c15706e63de56f66d48db69b686168e698637b5b
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-01 22:57:40 +0100

    Model3D model created

---
+++ app/Models/Model3D.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Model3D extends Model
+{
+    use HasFactory;
+
+    protected $table = 'models_3d';
+}
