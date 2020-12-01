4016ec05c7e06fbb0a7b48cc64bb238723a564b2
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-01 20:47:59 +0100

    deleted styles loads which was crashing some navbar functionality

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,8 +18,6 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ mix('css/app.css') }}" />
-    <script defer src="{{ mix('js/app.js') }}"></script>
 </head>
 <body>
     <div id="app">
