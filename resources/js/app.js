290c4be0ae84176789dd01b71ce0249b2dca5283
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-01 20:44:43 +0100

    Home component added

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,7 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('welcome', require('./components/Welcome.vue').default);
+Vue.component('home', require('./components/Home.vue').default);
 
 /**
  * Next, we will create a fresh Vue application instance and attach it to
