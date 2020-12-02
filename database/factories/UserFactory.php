89c03da37ce0fbaf89dca506b78ebd9815b1b7b7
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-02 17:56:46 +0100

    set root_dir value for fake users

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -26,7 +26,8 @@ class UserFactory extends Factory
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password,
+            'root_dir'=>substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 9),
             'remember_token' => Str::random(10),
         ];
     }
