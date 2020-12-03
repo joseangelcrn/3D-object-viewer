5f7e9c44a3b8d8c6ad51773ad3da8d89d6521a3b
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-03 11:39:54 +0100

    Component refactorized and now all data is read from user json

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -11,11 +11,11 @@
             </div>
         </div>
         <div class="row d-flex justify-content-center">
-            <div class="col-lg-3 col-md-3 border rounded  m-1 py-2" v-for="(object,index) in objects" :key="index">
-                <h3 class="text-center">{{object.title}}</h3>
-                <p class="text-center">{{object.description}}</p>
+            <div class="col-lg-3 col-md-3 border rounded  m-1 py-2" v-for="(model,index) in user.models" :key="index">
+                <h3 class="text-center">{{model.title}}</h3>
+                <p class="text-center">{{model.description}}</p>
                 <div class="text-center">
-                    <a href="#" class="btn btn-primary btn-sm">Ver</a>
+                    <a :href="'/model/'+model.id" class="btn btn-primary btn-sm">Ver</a>
                     <a href="#" class="btn btn-danger btn-sm">Borrar</a>
                 </div>
             </div>
@@ -28,56 +28,11 @@
         props:['user'],
         data(){
             return{
-                objects:[
-                    {
-                        title:'Titulo 3d',
-                        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam illum laudantium iure! Praesentium accusantium quas atque dolor accusamus obcaecati quisquam, ratione molestiae aliquid. Eius esse eos ipsa culpa, nisi ullam.'
-                    },
-                    {
-                        title:'Titulo 3d',
-                        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam illum laudantium iure! Praesentium accusantium quas atque dolor accusamus obcaecati quisquam, ratione molestiae aliquid. Eius esse eos ipsa culpa, nisi ullam.'
-                    },
-                    {
-                        title:'Titulo 3d',
-                        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam illum laudantium iure! Praesentium accusantium quas atque dolor accusamus obcaecati quisquam, ratione molestiae aliquid. Eius esse eos ipsa culpa, nisi ullam.'
-                    },
-                    {
-                        title:'Titulo 3d',
-                        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam illum laudantium iure! Praesentium accusantium quas atque dolor accusamus obcaecati quisquam, ratione molestiae aliquid. Eius esse eos ipsa culpa, nisi ullam.'
-                    },
-                    {
-                        title:'Titulo 3d',
-                        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam illum laudantium iure! Praesentium accusantium quas atque dolor accusamus obcaecati quisquam, ratione molestiae aliquid. Eius esse eos ipsa culpa, nisi ullam.'
-                    },
-                    {
-                        title:'Titulo 3d',
-                        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam illum laudantium iure! Praesentium accusantium quas atque dolor accusamus obcaecati quisquam, ratione molestiae aliquid. Eius esse eos ipsa culpa, nisi ullam.'
-                    },
-                    {
-                        title:'Titulo 3d',
-                        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam illum laudantium iure! Praesentium accusantium quas atque dolor accusamus obcaecati quisquam, ratione molestiae aliquid. Eius esse eos ipsa culpa, nisi ullam.'
-                    },
-                    {
-                        title:'Titulo 3d',
-                        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam illum laudantium iure! Praesentium accusantium quas atque dolor accusamus obcaecati quisquam, ratione molestiae aliquid. Eius esse eos ipsa culpa, nisi ullam.'
-                    },
-                    {
-                        title:'Titulo 3d',
-                        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam illum laudantium iure! Praesentium accusantium quas atque dolor accusamus obcaecati quisquam, ratione molestiae aliquid. Eius esse eos ipsa culpa, nisi ullam.'
-                    },
-                    {
-                        title:'Titulo 3d',
-                        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam illum laudantium iure! Praesentium accusantium quas atque dolor accusamus obcaecati quisquam, ratione molestiae aliquid. Eius esse eos ipsa culpa, nisi ullam.'
-                    },
-                    {
-                        title:'Titulo 3d',
-                        description:'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam illum laudantium iure! Praesentium accusantium quas atque dolor accusamus obcaecati quisquam, ratione molestiae aliquid. Eius esse eos ipsa culpa, nisi ullam.'
-                    },
-                ]
             }
         },
         mounted() {
             console.log('Component mounted.')
+            console.log(this.$props.user);
         }
     }
 </script>
