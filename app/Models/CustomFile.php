b4bbec98b1e1aa90fec03108fdabc176e5de9049
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-03 11:09:56 +0100

    Implementation of new function such as remove and exists (Pending of check if work correctly)

--- app/Models/CustomFile.php
+++ app/Models/CustomFile.php
@@ -7,13 +7,36 @@ use Illuminate\Support\Facades\Storage;
 
 class CustomFile
 {
-    public static function store($file){
+
+    public const  MODELS_ROOT_DIR = 'models3D';
+
+
+    public static function defaultDisk()
+    {
+        return Storage::disk('public');
+    }
+
+
+    public static function store($file,$test = false){
         $uniqueFileName = uniqid().'_'.$file->getClientOriginalName();
-        $stored =  Storage::disk('public')->put("objects3d\\".$uniqueFileName,  File::get($file));
+        $stored =  self::defaultDisk()->put(self::MODELS_ROOT_DIR."\\".$uniqueFileName,  File::get($file));
         $result = [
             'stored' => $stored,
             'unique_file_name'=>$uniqueFileName
         ];
         return $result;
+    }
+
+    public static function remove(string  $filename)
+    {
+        $removed = self::defaultDisk()->delete(self::MODELS_ROOT_DIR."\\".$filename);
+
+        return $removed;
+    }
+
+    public static function exists(string $path)
+    {
+        $exists = self::defaultDisk()->exists($path);
+        return $exists;
     }
 }
