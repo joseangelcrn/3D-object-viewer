b248643fb6540a9758a0f045094f43c6e14ec6e0
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-03 16:20:15 +0100

    model shot and 3dviewer created

---
+++ resources/js/components/Model3DShow.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <h1>{{model.title}}</h1>
+                <p class="bg-secondary p-2 rounded text-white mt-3">{{model.description}}</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <model3d-viewer :path="'models3D\\'+model.file_name"></model3d-viewer>
+            </div>
+        </div>
+    </div>
+</template>
+<style>
+</style>
+<script>
+    export default {
+        props:['model'],
+        mounted() {
+            console.log('Component mounted.')
+            console.log(this.$props.model);
+        }
+    }
+</script>
---
+++ resources/js/components/Model3DViewer.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="container">
+       3d viewer
+    </div>
+</template>
+
+<script>
+    export default {
+        props:['path'],
+        data(){
+            return{
+            }
+        },
+        mounted() {
+            console.log('3D viewer montado !.');
+            console.log(this.$props.path);
+        }
+    }
+</script>
