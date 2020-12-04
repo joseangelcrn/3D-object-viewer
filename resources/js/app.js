30c876aace829cbd1756c0f4d52271a8fa019050
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-04 14:26:06 +0100

    vue confirm dialog using

--- resources/js/app.js
+++ resources/js/app.js
@@ -9,15 +9,20 @@ require('./bootstrap');
 window.Vue = require('vue');
 
 /**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
+ * Packages
  */
+import VueConfirmDialog from 'vue-confirm-dialog'
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
+
+Vue.use(VueConfirmDialog)
+
+
+Vue.component('confirm-dialog', VueConfirmDialog.default)
+
+
+/**
+ *Components
+ */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('welcome', require('./components/Welcome.vue').default);
