7c8d3087fc0a69d00ebfd2fe416c57202eb283bf
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-04 14:25:32 +0100

    Refactoring and confirm dialog implementing

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -21,32 +21,14 @@
                             </div>
                             <div class="col-lg-4 col-md-6">
                                 <a :href="'/model/'+model.id" class="btn btn-primary btn-sm" :class="[sending ? 'disabled' : '']">Ver</a>
-                                <a  @click="deleteModel(model.id,index)" class="btn btn-danger btn-sm" :class="[sending ? 'disabled' : '']">Borrar</a>
+                                <a  @click="deleteModelWithConfirm(model.id,index)" class="btn btn-danger btn-sm" :class="[sending ? 'disabled' : '']">Borrar</a>
                             </div>
                         </div>
                     </li>
                 </ul>
             </div>
         </div>
-        <!-- <div class="row d-flex justify-content-center">
-            <div class="col-lg-3 col-md-3 border rounded  m-1 py-2" v-for="(model,index) in data_user.models" :key="index">
-                <div class="row">
-                    <div class="col-12">
-                        <h3 class="text-center">{{model.title}}</h3>
-                        <p class="text-center">{{model.description}}</p>
-                    </div>
-                </div>
-               <div class="row justify-content-center bg-blue flex-grow-1">
-                    <div class="    col-lg-12 col-md-12" >
-                        <a :href="'/model/'+model.id" class="btn btn-primary btn-sm" :class="[sending ? 'disabled' : '']">Ver</a>
-                        <a  @click="deleteModel(model.id,index)" class="btn btn-danger btn-sm" :class="[sending ? 'disabled' : '']">Borrar</a>
-                    </div>
-               </div>
-            </div>
-        </div> -->
-<!--  -->
-
-
+    <vue-confirm-dialog></vue-confirm-dialog>
 
     </div>
 </template>
@@ -62,29 +44,54 @@
         },
         methods:{
             deleteModel(idModel,index){
-                let that = this;
-                this.sending = true;
-                axios.delete('model/'+idModel, {params: {'id': idModel}})
-                .then(
-                    (response)=>{
-                        console.log('respnse');
-                        console.log(response.status);
-                        if (response.status === 200) {
-                            that.deleteModelFromView(index);
-                        }
-                        that.sending = false;
-                    },
-                    (error)=>{
-                        console.log('error');
-                        console.log(error);
-                        that.sending = false;
+                    let that = this;
+                    this.sending = true;
+                    axios.delete('model/'+idModel, {params: {'id': idModel}})
+                    .then(
+                        (response)=>{
+                            console.log('respnse');
+                            console.log(response.status);
+                            if (response.status === 200) {
+                                that.deleteModelFromView(index);
+                            }
+                            that.sending = false;
+                        },
+                        (error)=>{
+                            console.log('error');
+                            console.log(error);
+                            that.sending = false;
 
-                    },
-                );
+                        },
+                    );
+
 
             },
-            deleteModelFromView(index){
+            deleteModelFromView(idModel,index){
                 this.data_user.models.splice(index,1)
+            },
+            deleteModelWithConfirm(idModel,index){
+                let modelTitle = this.data_user.models[index].title;
+                let message = "¿Estas seguro/a que deseas eliminar el modelo 3d con titulo: '"+modelTitle+"'?"
+                let that = this;
+
+                this.$confirm(
+                        {
+                        message,
+                        button: {
+                            no: 'No',
+                            yes: 'Si'
+                        },
+                        /**
+                         * Callback Function
+                         * @param {Boolean} confirm
+                         */
+                        callback: (confirm) => {
+                            if (confirm) {
+                                this.deleteModel(idModel,index);
+                            }
+                        }
+                        }
+                    )
             }
         },
         beforeMount(){
