59cbbcbffeb1ad48786916a0a566c22e03833fed
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-04 15:36:07 +0100

    setting root_dir field on create time

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -64,10 +64,12 @@ class RegisterController extends Controller
      */
     protected function create(array $data)
     {
+        $rootDir = substr(base_convert(sha1(uniqid(mt_rand())), 16, 36), 0, 9);
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'root_dir' => $rootDir
         ]);
     }
 }
