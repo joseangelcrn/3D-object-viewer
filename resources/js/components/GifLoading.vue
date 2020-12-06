ce8b12c7a44895910488e52c0166f9189f6b51ac
joseangelcrn <jose.angel.cabeza.rn@gmail.com>
2020-12-06 20:58:46 +0100

    Gif loading component to display loading moment to users

---
+++ resources/js/components/GifLoading.vue
@@ -0,0 +1,21 @@
+<template>
+    <div id="gif_loading">
+        <img id="gif_img"  :src="'../storage/system/loader.gif'" alt="Gif loading">
+    </div>
+</template>
+<style>
+    #gif_loading{
+        position: absolute;
+        right:0;
+        z-index: 1;
+    }
+    #gif_img{
+        width:75px;
+        height: 75px;
+    }
+</style>
+<script>
+    export default {
+
+    }
+</script>
